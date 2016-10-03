c04adc3d70853d2c6c5185bd8f71ccc8fe196f90
webkey <webkey_i@ukr.net>
2016-10-03 19:49:23 +0300

    office_20161003_1950

--- js/functions.js
+++ js/functions.js
@@ -45,6 +45,239 @@ function printShow() {
 /*print end*/
 
 /**!
+ * main navigation
+ * */
+(function ($) {
+	// external js:
+	// 1) TweetMax VERSION: 1.19.0 (widgets.js);
+	// 2) device.js 0.2.7 (widgets.js);
+	// 3) resizeByWidth (resize only width);
+	var MainNavigation = function (settings) {
+		var options = $.extend({
+			mainContainer: 'html',
+			navContainer: null,
+			navMenu: '.nav-list',
+			btnMenu: '.btn-menu',
+			navMenuItem: '.nav-list > li',
+			navMenuAnchor: 'a',
+			navDropMenu: '.js-nav-drop',
+			staggerItems: null,
+			overlayClass: '.nav-overlay',
+			overlayAppend: 'body',
+			overlayAlpha: 0.8,
+			classNoClickDrop: '.no-click', // Класс, при наличии которого дроп не буте открываться по клику
+			classReturn: null,
+			overlayBoolean: false,
+			animationSpeed: 300,
+			animationSpeedOverlay: null,
+			minWidthItem: 100
+		},settings || {});
+
+		var self = this,
+			container = $(options.navContainer),
+			_animateSpeed = options.animationSpeed;
+
+		self.options = options;
+		self.$mainContainer = $(options.mainContainer);            // Основной контейнер дом дерева. по умолчанию <html></html>
+		self.$navMenu = $(options.navMenu);
+		self.$btnMenu = $(options.btnMenu);                        // Кнопка открытия/закрытия меню для моб. верси;
+		self.$navContainer = container;
+		self.$navMenuItem = $(options.navMenuItem, container);     // Пункты навигации;
+		self.$navMenuAnchor = $(options.navMenuAnchor, container); // Элемент, по которому производится событие (клик);
+		self.$navDropMenu = $(options.navDropMenu, container);     // Дроп-меню всех уровней;
+		self.$staggerItems = options.staggerItems || self.$navMenuItem;  //Элементы в стеке, к которым применяется анимация. По умолчанию navMenuItem;
+
+		self._animateSpeed = _animateSpeed;
+		self._classNoClick = options.classNoClickDrop;
+
+		// overlay
+		self._overlayBoolean = options.overlayBoolean;            // Добавить оверлей (по-умолчанию == false). Если не true, то не будет работать по клику вне навигации;
+		self._overlayClass = options.overlayClass;                // Класс оверлея;
+		self.overlayAppend = options.overlayAppend;               // Элемент ДОМ, вконец которого добавится оверлей, по умолчанию <body></body>;
+		self.$overlay = $('<div class="' + self._overlayClass.substring(1) + '"></div>'); // Темплейт оверлея;
+		self._overlayAlpha = options.overlayAlpha;
+		self._animateSpeedOverlay = options.animationSpeedOverlay || _animateSpeed;
+		self._minWidthItem = options.minWidthItem;
+
+		self.desktop = device.desktop();
+
+		self.modifiers = {
+			active: 'active',
+			opened: 'nav-opened',
+			openStart: 'nav-opened-start'
+		};
+
+		self.createOverlay();
+		self.toggleNav();
+		self.clearStyles();
+	};
+
+	MainNavigation.prototype.navIsOpened = false;
+
+	// init tween animation
+	MainNavigation.prototype.overlayTween = new TimelineMax({paused: true});
+
+	// overlay append to "overlayAppend"
+	MainNavigation.prototype.createOverlay = function () {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var $overlay = self.$overlay;
+		$overlay.appendTo(self.overlayAppend);
+
+		TweenMax.set($overlay, {autoAlpha: 0});
+
+		self.overlayTween.to($overlay, self._animateSpeedOverlay / 1000, {autoAlpha: self._overlayAlpha});
+	};
+
+	// show/hide overlay
+	MainNavigation.prototype.showOverlay = function (close) {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var overlayTween = self.overlayTween;
+
+		if (close === false) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		if (overlayTween.progress() != 0 && !overlayTween.reversed()) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		overlayTween.play();
+	};
+
+	// switch nav
+	MainNavigation.prototype.toggleNav = function () {
+		var self = this,
+			$buttonMenu = self.$btnMenu;
+
+		self.preparationAnimation();
+
+		$buttonMenu.on('click', function (e) {
+			if (self.navIsOpened) {
+				self.closeNav();
+			} else {
+				self.openNav();
+			}
+
+			e.preventDefault();
+		});
+
+		$(document).on('click', self._overlayClass, function () {
+			self.closeNav();
+		});
+	};
+
+	// open nav
+	MainNavigation.prototype.openNav = function() {
+		var self = this,
+			$html = self.$mainContainer,
+			$navContainer = self.$navContainer,
+			$buttonMenu = self.$btnMenu,
+			_animationSpeed = self._animateSpeedOverlay,
+			$staggerItems = self.$staggerItems;
+
+		$buttonMenu.addClass(self.modifiers.active);
+		$html.addClass(self.modifiers.openStart);
+
+		$navContainer.css({
+			'-webkit-transition-duration': '0s',
+			'transition-duration': '0s'
+		});
+
+		var navTween = new TimelineMax();
+
+		navTween
+			.to($navContainer, _animationSpeed / 1000, {
+				yPercent: 0, onComplete: function () {
+					$html.addClass(self.modifiers.opened);
+					TweenMax.staggerTo($staggerItems, 0.3, {autoAlpha:1, scale:1, ease:Cubic.easeInOut}, 0.08);
+				}, ease:Cubic.easeInOut
+			});
+
+		self.showOverlay();
+
+		self.navIsOpened = true;
+	};
+
+	// close nav
+	MainNavigation.prototype.closeNav = function() {
+		var self = this,
+			$html = self.$mainContainer,
+			$navContainer = self.$navContainer,
+			$buttonMenu = self.$btnMenu,
+			_animationSpeed = self._animateSpeedOverlay;
+
+		$html.removeClass(self.modifiers.opened);
+		$html.removeClass(self.modifiers.openStart);
+		$buttonMenu.removeClass(self.modifiers.active);
+
+		self.showOverlay(false);
+
+		TweenMax.to($navContainer, _animationSpeed / 1000, {
+			yPercent: 120, onComplete: function () {
+				self.preparationAnimation();
+			}
+		});
+
+		self.navIsOpened = false;
+	};
+
+	// preparation element before animation
+	MainNavigation.prototype.preparationAnimation = function() {
+		var self = this,
+			$navContainer = self.$navContainer,
+			$staggerItems = self.$staggerItems,
+			$btnMenu = self.$btnMenu;
+
+		if ($btnMenu.is(':visible')) {
+			TweenMax.set($navContainer, {yPercent: 120, onComplete: function () {
+				$navContainer.show(0);
+			}});
+			TweenMax.set($staggerItems, {autoAlpha: 0, scale: 0.8});
+		}
+	};
+
+	// clearing inline styles
+	MainNavigation.prototype.clearStyles = function() {
+		var self = this,
+			$btnMenu = self.$btnMenu,
+			$navContainer = self.$navContainer,
+			$staggerItems = self.$staggerItems;
+
+		//clear on horizontal resize
+		$(window).on('resizeByWidth', function () {
+			if (!$btnMenu.is(':visible')) {
+				$navContainer.attr('style', '');
+				$staggerItems.attr('style', '');
+			} else {
+				self.closeNav();
+			}
+		});
+	};
+
+	window.MainNavigation = MainNavigation;
+
+}(jQuery));
+
+function mainNavigationInit(){
+	var $container = $('.nav');
+	if(!$container.length){ return; }
+	new MainNavigation({
+		navContainer: $container,
+		overlayAppend: '.wrapper',
+		animationSpeed: 300,
+		overlayBoolean: true,
+		overlayAlpha: 0.75
+	});
+}
+/*main navigation end*/
+
+/**!
  * sliders
  * */
 function slidersInit() {
@@ -60,7 +293,13 @@ function slidersInit() {
 				slidesToScroll: 1,
 				infinite: true,
 				dots: true,
-				arrows: false
+				arrows: false,
+				responsive: [
+					{
+						breakpoint: 3000,
+						// settings: "unslick"
+					}
+				]
 			});
 		});
 	}
@@ -94,7 +333,30 @@ function slidersInit() {
 				slidesToScroll: 3,
 				infinite: true,
 				dots: false,
-				arrows: true
+				arrows: true,
+				responsive: [
+					{
+						breakpoint: 1280,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 600,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
 			});
 		});
 	}
@@ -149,6 +411,7 @@ function footerBottom(){
 $(document).ready(function(){
 	placeholderInit();
 	printShow();
+	mainNavigationInit();
 	slidersInit();
 
 	equalHeightMainBlocks();
